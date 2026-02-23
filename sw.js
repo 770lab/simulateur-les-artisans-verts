7220f700b943deb1f5f3e45a738d2b087ed223fa
770lab <chabadclub770@gmail.com>
2026-02-23 19:08:50 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Les Artisans Verts © 2026
 // ============================================
 
-const CACHE_NAME = 'pac-sim-v11';
+const CACHE_NAME = 'pac-sim-v13';
 const ASSETS = [
   './',
   './index.html',
